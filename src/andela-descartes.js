9c66af68b45ac1b2448c0ae6c27263dc8cb7653c
Innocent <innocent.amadi@andela.com>
2016-03-30 14:41:16 +0100

    changes from sent

--- src/andela-descartes.js
+++ src/andela-descartes.js
@@ -1,23 +1,53 @@
-var styles = {
-	"html": {
-		"margin": 0,
-		"padding": 0,
-		"body": {
-			"color": "red",
-			"font-family": "Helvetica"
+'use strict'
+
+const Descartes = (tree) => {
+	var check = applyMixins(tree);
+	var objStyles = flatten(tree);
+	applyStyles(objStyles);
+}
+
+let applyStyles = (objStyles) => {
+	var elements, selector, style, lastIndex;
+	for (var key in objStyles) {
+		lastIndex = key.lastIndexOf(' ');
+		selector = key.substring(0, lastIndex);
+		style = key.substring(lastIndex).trim();
+		elements = document.querySelectorAll(selector);
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].style[style] = objStyles[key];
 		}
 	}
 }
 
-var html = $(document).find('html');
-var rulesStr;
+let flatten = (obj) => {
+  let builtObj = {}
+  for (var key in obj) {
+    flattenNode(obj, key, builtObj)
+  }
+  console.log(builtObj)
+  return builtObj;
+}
 
-const flatten = (obj) => {
-  var dummyObj = {}, rules;
-  for (key in obj) {
-    var rules = obj[key];
-    if (typeof rules == 'object') {
-      flatten(rules)
-    }
+let applyMixins = (obj, parentKey) => {
+	for (var key in obj) {
+		if (key === "_mixin")  {
+			obj[parentKey] = obj[key];
+		} else if (typeof key === 'object') {
+			debugger;
+			applyMixins(obj[key], key)
+		}
+	}
+	return obj;
+}
+
+let flattenNode = (obj, key, builtObj, parentKey, str) => {
+  str = str || ""
+  str += ((str.length > 0) ? " " + key : key)
+  if (typeof obj[key] == 'object'){
+		for (var k in obj[key]) {
+			flattenNode(obj[key], k, builtObj, key, str)
+		}
+  } else {
+      builtObj[str] = obj[key]
   }
 }
--- src/style.js
+++ src/style.js
@@ -1,10 +1,21 @@
-var styles = {
-	"html": {
-		"margin": 0,
-		"padding": 0,
-		"body": {
-			"color": "red",
-			"font-family": "Helvetica"
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	var _font = {
+		"color": "#666",
+		"font-family": "Helvetica",
+		'p': {
+			'font-weight': 'normal'
 		}
 	}
-}
+	var styles = {
+		"html": {
+			"margin": 0,
+			"padding": 0,
+			"body": {
+				'_mixin': _font
+			}
+		}
+	}
+	Descartes(styles)
+});
