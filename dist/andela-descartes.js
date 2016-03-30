5433f5c9de7504e19aa5585146b049d985186799
Jon Chan <me@jonhmchan.com>
2016-03-30 10:00:42 -0400

    Add mixins application

--- dist/andela-descartes.js
+++ dist/andela-descartes.js
@@ -1,57 +1,60 @@
-"use strict";
+'use strict';
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var Descartes = function Descartes(tree) {
+	// var check = applyMixins(tree);
+	var objStyles = flatten(tree);
+	applyStyles(objStyles);
+};
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Andela = function () {
-	function Andela(styles) {
-		_classCallCheck(this, Andela);
-
-		this.styles = styles;
-		this.mapping = {};
-		this.rulesStr;
-		this.flatten();
-		console.log(this.mapping);
-	}
-
-	_createClass(Andela, [{
-		key: "flatten",
-		value: function flatten() {
-			var obj = arguments.length <= 0 || arguments[0] === undefined ? this.styles : arguments[0];
-			var selector = arguments.length <= 1 || arguments[1] === undefined ? null : arguments[1];
-
-			var dummyObj = {},
-			    rules;
-			for (var key in obj) {
-				var rules = obj[key];
-				if ((typeof rules === "undefined" ? "undefined" : _typeof(rules)) === 'object') {
-					// This is a nested rule, run flatten again
-					this.flatten(rules, key);
-				} else {
-					// This is a real CSS rule
-					var rule = {};
-					rule[key] = rules;
-					this.mapping[selector] = rule;
-				}
-			}
-			return dummyObj;
-		}
-	}]);
-
-	return Andela;
-}();
-
-var styles = {
-	"html": {
-		"margin": 0,
-		"padding": 0,
-		"body": {
-			"color": "red",
-			"font-family": "Helvetica"
+var applyStyles = function applyStyles(objStyles) {
+	var elements, selector, style, lastIndex;
+	for (var key in objStyles) {
+		lastIndex = key.lastIndexOf(' ');
+		selector = key.substring(0, lastIndex);
+		style = key.substring(lastIndex).trim();
+		elements = document.querySelectorAll(selector);
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].style[style] = objStyles[key];
 		}
 	}
 };
-new Andela(styles);
+
+var flatten = function flatten(obj) {
+	var builtObj = {};
+	for (var key in obj) {
+		flattenNode(obj, key, builtObj);
+	}
+	console.log(builtObj);
+	return builtObj;
+};
+
+// Create a new tree that applies all mixins before flattening
+var applyMixins = function applyMixins(tree) {
+	// Apply the mixins
+	var obj = {};
+
+	/*
+ if (key === '_mixin') {
+  		var mixin = obj[key]
+  		for (var property in mixin) {
+  			obj[property] = mixin[property]
+  		}
+  	}
+  	*/
+
+	return obj;
+};
+
+var flattenNode = function flattenNode(obj, key, builtObj, str) {
+	str = str || "";
+	str += str.length > 0 ? " " + key : key;
+	if (_typeof(obj[key]) == 'object') {
+		for (var k in obj[key]) {
+			flattenNode(obj[key], k, builtObj, str);
+		}
+	} else {
+		builtObj[str] = obj[key];
+	}
+};
--- src/andela-descartes.js
+++ src/andela-descartes.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 'use strict'
 
 const Descartes = (tree) => {
-	var check = applyMixins(tree);
+	// var check = applyMixins(tree);
 	var objStyles = flatten(tree);
 	applyStyles(objStyles);
 }
@@ -29,66 +28,31 @@ let flatten = (obj) => {
   return builtObj;
 }
 
-let applyMixins = (obj, parentKey) => {
-	for (var key in obj) {
-		if (key === "_mixin")  {
-			obj[parentKey] = obj[key];
-		} else if (typeof key === 'object') {
-			debugger;
-			applyMixins(obj[key], key)
-		}
-	}
-	return obj;
+// Create a new tree that applies all mixins before flattening
+let applyMixins = (tree) => {
+	// Apply the mixins
+	var obj = {}
+
+	/*
+	if (key === '_mixin') {
+  		var mixin = obj[key]
+  		for (var property in mixin) {
+  			obj[property] = mixin[property]
+  		}
+  	}
+  	*/
+
+	return obj
 }
 
-let flattenNode = (obj, key, builtObj, parentKey, str) => {
+let flattenNode = (obj, key, builtObj, str) => {
   str = str || ""
   str += ((str.length > 0) ? " " + key : key)
   if (typeof obj[key] == 'object'){
-		for (var k in obj[key]) {
-			flattenNode(obj[key], k, builtObj, key, str)
-		}
+	for (var k in obj[key]) {
+		flattenNode(obj[key], k, builtObj, str)
+	}
   } else {
       builtObj[str] = obj[key]
   }
 }
-=======
-class Andela {
-	constructor(styles) {
-		this.styles = styles
-		this.mapping = {}
-		this.rulesStr;
-		this.flatten()
-		console.log(this.mapping)
-	}
-
-	flatten(obj = this.styles, selector = null) {
-		var dummyObj = {}, rules;
-		for (var key in obj) {
-			var rules = obj[key];
-		    if (typeof rules === 'object') {
-		    	// This is a nested rule, run flatten again
-		    	this.flatten(rules, key)
-		    } else {
-		    	// This is a real CSS rule
-		    	var rule = {}
-		    	rule[key] = rules
-		    	this.mapping[selector] = rule
-		    }
-		}
-		return dummyObj
-  	}
-}
-
-var styles = {
-	"html": {
-		"margin": 0,
-		"padding": 0,
-		"body": {
-			"color": "red",
-			"font-family": "Helvetica"
-		}
-	}
-}
-new Andela(styles)
->>>>>>> 17158e2f69c1abca2c86bb3638cdaf8500c84641
